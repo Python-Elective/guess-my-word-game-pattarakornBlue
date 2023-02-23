8f22ece51e1f1c4bb50af91be643b1c237b1f1fc
Blue <pattarakorn.ba2024@kmids.ac.th>
2023-02-23 07:38:27 +0700

    1

---
+++ Blue-1104-high_count.py
@@ -0,0 +1,20 @@
+# collab with poon
+
+"""
+for all .values() in the dictionary
+    get the len(values)
+    compare the values
+    if value greater than the previous value on list
+        replace the value
+    if equal
+        keep both
+
+"""
+
+def high_count(dic):
+    count = 0
+    max_value = []
+    
+aDict = {'k': [100, 12, 12, 16], 'M': [1, 'test']} 
+
+print(high_count(aDict)) 
